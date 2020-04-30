4b9744589af5f850d8ab6d8f9b6914e821374f79
hwwilliams <20447447+hwwilliams@users.noreply.github.com>
2020-04-29 23:30:52 -0400

    add fucntion for check logs

--- ssh-login-notify.py
+++ ssh-login-notify.py
@@ -4,17 +4,18 @@ import logging
 
 # from systemd.journal import JournaldLogHandler
 from twilio_notifications.messenger import TwilioNotification
+from log_processor.check import Check
 
 
 def configure_logging():
     logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)s : %(name)s : %(message)s')
     logger = logging.getLogger('__main__')
-    # journald_handler = JournaldLogHandler()
-    # journald_handler.setFormatter(logging.Formatter(
-    #     '[%(levelname)s] %(message)s'
-    # ))
-    # logger.addHandler(journald_handler)
+    journald_handler = JournaldLogHandler()
+    journald_handler.setFormatter(logging.Formatter(
+        '[%(levelname)s] %(message)s'
+    ))
+    logger.addHandler(journald_handler)
 
     twilio_http_client_logger = logging.getLogger('twilio.http_client')
     twilio_http_client_logger.setLevel(logging.WARNING)
@@ -28,9 +29,15 @@ def twilio_notifications(message_to_send):
     client.process_messasge(message_to_send)
 
 
+def check_log():
+    processor = Check()
+    return processor.poll()
+
+
 def main():
     configure_logging()
-    twilio_notifications('Test Message')
+    log_line = check_log
+    twilio_notifications(log_line)
 
 
 if __name__ == "__main__":
