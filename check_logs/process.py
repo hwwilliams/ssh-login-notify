c13c86625cf6e5b67970a8eca71b8c9b84dd29e6
hwwilliams <20447447+hwwilliams@users.noreply.github.com>
2020-04-30 00:20:00 -0400

    introduce more logging

--- check_logs/process.py
+++ check_logs/process.py
@@ -24,7 +24,7 @@ def get_log_file():
         os.path.dirname(__file__), '..', 'settings', 'log_file.json'))
 
     logger.debug(
-        f'Attempting to load ssh log file from: "{log_file_path}".')
+        f'Attempting to load log file path from: "{log_file_path}".')
 
     try:
         with open(log_file_path, 'r') as file:
@@ -32,7 +32,7 @@ def get_log_file():
 
     except json.JSONDecodeError:
         logger.error(
-            f'No valid JSON data found when attempting to load ssh log file from: "{log_file_path}".')
+            f'No valid JSON data found when attempting to load log file path from: "{log_file_path}".')
         raise
 
     except FileNotFoundError:
@@ -42,7 +42,7 @@ def get_log_file():
 
     if 'log_path' in log_dict[0]:
         logger.debug(
-            f'Successfully loaded contact information from: "{log_file_path}".')
+            f'Successfully loaded log file path from: "{log_file_path}".')
         return (log_dict[0]['log_path'])
 
     else:
@@ -58,19 +58,46 @@ def twilio_notification(message_to_send):
 
 class Process:
     def __init__(self):
+        logger.info(
+            f'Attempting to gather log file location for processing.')
 
-        self.log_file = get_log_file()
+        (
+            self.log_file,
+            self.log_file_path
+        ) = get_log_file()
+
+        logger.info(
+            f'Successfully gathered log file location for processing.')
 
     def poll(self):
+        logger.info(
+            f'Attempting initilize connection to log file for processing.')
 
-        open_file = subprocess.Popen(["tail", "-F", "-n", "0", self.log_file],
-                                     encoding="utf8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        poller_object = select.poll()
-        poller_object.register(open_file.stdout)
+        try:
+            open_file = subprocess.Popen(["tail", "-F", "-n", "0", self.log_file],
+                                         encoding="utf8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            poller_object = select.poll()
+            poller_object.register(open_file.stdout)
 
-        while True:
-            if poller_object.poll(1):
-                log_line = open_file.stdout.readline()
-                if all(entry in log_line for entry in ('sshd:session' and 'session opened')):
-                    twilio_notification(log_line)
-            time.sleep(1)
+        except PermissionError as e:
+            logger.error(
+                f'Failed to open log file for processing due to invalid file permissions from: "{self.log_file_path}".')
+            logger.error(e)
+
+        else:
+            logger.info(
+                f'Successfully initilized connection to log file for processing.')
+
+            logger.info(
+                f'Waiting for new log activity to process.')
+
+            while True:
+
+                if poller_object.poll(1):
+                    log_line = open_file.stdout.readline()
+
+                    if all(entry in log_line for entry in ('sshd:session' and 'session opened')):
+                        logger.debug('SSH activity detected.')
+                        twilio_notification(log_line)
+
+                time.sleep(1)
